2a7417baf6f04eaeec156d0709f4075a1b12f1ca
VYKETETRORP <lesleygs56@gamil.com>
2024-12-20 16:56:25 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+{{name}} -{{ age }}  </div>
+<button @click="changeName('Mama')" >
+
+change name
+</button>
+<button @click="changeAge(49)">change age</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import {ref} from 'vue'
+// import HelloWorld from './components/HelloWorld.vue';
+
+// export default defineComponent({
+//   name: 'App',
+//   components: {
+
+//   },
+//   data (){
+//     return {
+//       name:'LInk',
+//       age:30,
+
+//     }
+//   },
+//   methods : {
+//     changeName(name:string){
+//       this.name=name
+//     },
+//     changeAge(age:number){
+//       this.age=age
+//     }
+
+//   }
+// });
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+    const name = ref<string>('KEEE')
+    const age = ref<number|string>(5)
+
+    const changeName = (newName : string)=>{
+      name.value=newName
+
+    }
+    const changeAge = (newAge : number)=>{
+      age.value=newAge
+    }
+    return{
+      changeName,changeAge,
+      name,age,
+    }
+
   }
-});
+})
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
